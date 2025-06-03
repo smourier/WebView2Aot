7f186d1ec240b76e56eb13f3588a88e6f8285114
Simon Mourier <smourier@users.noreply.github.com>
2025-06-03 23:35:23 +0200

    Added script execution utilities.

---
+++ WebView2/Utilities/CoreWebView2ScriptException.cs
@@ -0,0 +1,62 @@
+﻿namespace WebView2.Utilities;
+
+[Serializable]
+public class CoreWebView2ScriptException : Exception
+{
+    public CoreWebView2ScriptException()
+    {
+    }
+
+    public CoreWebView2ScriptException(string message)
+        : base(message)
+    {
+    }
+
+    public CoreWebView2ScriptException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public CoreWebView2ScriptException(string message, uint lineNumber, uint columnNumber, string name, string json)
+    : base(message)
+    {
+        LineNumber = lineNumber;
+        ColumnNumber = columnNumber;
+        Name = name;
+        Json = json;
+    }
+
+    public string? Name { get; }
+    public uint LineNumber { get; }
+    public uint ColumnNumber { get; }
+    public string? Json { get; }
+
+    public static CoreWebView2ScriptException? From(ICoreWebView2ScriptException? error)
+    {
+        if (error == null)
+            return null;
+
+        uint lineNumber = 0;
+        error.get_LineNumber(ref lineNumber).ThrowOnError();
+        var message = PWSTR.Null;
+        error.get_Message(ref message).ThrowOnError();
+        var name = PWSTR.Null;
+        error.get_Name(ref name).ThrowOnError();
+        uint columnNumber = 0;
+        error.get_ColumnNumber(ref columnNumber).ThrowOnError();
+        var json = PWSTR.Null;
+        error.get_ToJson(ref json).ThrowOnError();
+        var exception = new CoreWebView2ScriptException(
+            message.ToString() ?? "Error",
+            lineNumber,
+            columnNumber,
+            name.ToString() ?? string.Empty,
+            json.ToString() ?? string.Empty
+        );
+        Marshal.FreeCoTaskMem(message.Value);
+        Marshal.FreeCoTaskMem(name.Value);
+        Marshal.FreeCoTaskMem(json.Value);
+        return exception;
+    }
+}
+
---
+++ WebView2/Utilities/ICoreWebView2Extensions.cs
@@ -0,0 +1,133 @@
+﻿namespace WebView2.Utilities;
+
+public static class ICoreWebView2Extensions
+{
+    public static Task<string?> ExecuteScriptAsJon(this ICoreWebView2 webView, string javaScript)
+    {
+        ArgumentNullException.ThrowIfNull(webView);
+        ArgumentNullException.ThrowIfNull(javaScript);
+
+        var tcs = new TaskCompletionSource<string?>();
+        HRESULT hr;
+        if (webView is not ICoreWebView2_21 wv21)
+        {
+            hr = webView.ExecuteScript(PWSTR.From(javaScript), new CoreWebView2ExecuteScriptCompletedHandler((result, jsonPtr) =>
+            {
+                if (jsonPtr.Value == 0)
+                {
+                    tcs.SetResult(null);
+                    return;
+                }
+
+                var json = jsonPtr.ToString()!;
+                Marshal.FreeCoTaskMem(jsonPtr.Value);
+                tcs.SetResult(json);
+            }));
+        }
+        else
+        {
+            hr = wv21.ExecuteScriptWithResult(PWSTR.From(javaScript), new CoreWebView2ExecuteScriptWithResultCompletedHandler((result, p) =>
+            {
+                var succeeded = BOOL.FALSE;
+                p.get_Succeeded(ref succeeded);
+                if (!succeeded)
+                {
+                    p.get_Exception(out var error);
+                    var exception = CoreWebView2ScriptException.From(error);
+                    if (exception != null)
+                    {
+                        tcs.SetException(exception);
+                    }
+                    else
+                    {
+                        tcs.SetResult(null);
+                    }
+                    return;
+                }
+
+                var jsonPtr = PWSTR.Null;
+                p.get_ResultAsJson(ref jsonPtr);
+                if (jsonPtr.Value == 0)
+                {
+                    tcs.SetResult(null);
+                    return;
+                }
+
+                var json = jsonPtr.ToString()!;
+                Marshal.FreeCoTaskMem(jsonPtr.Value);
+                tcs.SetResult(json);
+            }));
+        }
+
+        if (hr.IsError)
+        {
+            tcs.SetException(Marshal.GetExceptionForHR(hr)!);
+        }
+        return tcs.Task;
+    }
+
+    public static Task<T?> ExecuteScript<T>(this ICoreWebView2 webView, string javaScript, JsonTypeInfo<T> typeInfo, T? defaultValue = default)
+    {
+        ArgumentNullException.ThrowIfNull(webView);
+        ArgumentNullException.ThrowIfNull(javaScript);
+        ArgumentNullException.ThrowIfNull(typeInfo);
+
+        var tcs = new TaskCompletionSource<T?>();
+        HRESULT hr;
+        if (webView is not ICoreWebView2_21 wv21)
+        {
+            hr = webView.ExecuteScript(PWSTR.From(javaScript), new CoreWebView2ExecuteScriptCompletedHandler((result, jsonPtr) =>
+            {
+                if (jsonPtr.Value == 0)
+                {
+                    tcs.SetResult(defaultValue);
+                    return;
+                }
+
+                var json = jsonPtr.ToString()!;
+                Marshal.FreeCoTaskMem(jsonPtr.Value);
+                tcs.SetResult(JsonSerializer.Deserialize(json, typeInfo) ?? defaultValue);
+            }));
+        }
+        else
+        {
+            hr = wv21.ExecuteScriptWithResult(PWSTR.From(javaScript), new CoreWebView2ExecuteScriptWithResultCompletedHandler((result, p) =>
+            {
+                var succeeded = BOOL.FALSE;
+                p.get_Succeeded(ref succeeded);
+                if (!succeeded)
+                {
+                    p.get_Exception(out var error);
+                    var exception = CoreWebView2ScriptException.From(error);
+                    if (exception != null)
+                    {
+                        tcs.SetException(exception);
+                    }
+                    else
+                    {
+                        tcs.SetResult(defaultValue);
+                    }
+                    return;
+                }
+
+                var jsonPtr = PWSTR.Null;
+                p.get_ResultAsJson(ref jsonPtr);
+                if (jsonPtr.Value == 0)
+                {
+                    tcs.SetResult(defaultValue);
+                    return;
+                }
+
+                var json = jsonPtr.ToString()!;
+                Marshal.FreeCoTaskMem(jsonPtr.Value);
+                tcs.SetResult(JsonSerializer.Deserialize(json, typeInfo) ?? defaultValue);
+            }));
+        }
+
+        if (hr.IsError)
+        {
+            tcs.SetException(Marshal.GetExceptionForHR(hr)!);
+        }
+        return tcs.Task;
+    }
+}
