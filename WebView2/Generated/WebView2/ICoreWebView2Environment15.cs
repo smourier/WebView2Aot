db10ebf5143fe6e72036bda754cf072bdb67b6fd
Simon Mourier <smourier@users.noreply.github.com>
2025-09-13 16:37:33 +0200

    Updated to latest webview SDK. Breaking change replace <ref string> by <out string> when possible.

---
+++ WebView2/Generated/WebView2/ICoreWebView2Environment15.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+namespace WebView2;
+
+[GeneratedComInterface, Guid("2ac5ebfb-e654-5961-a667-7971885c7b27")]
+public partial interface ICoreWebView2Environment15 : ICoreWebView2Environment14
+{
+    [PreserveSig]
+    [return: MarshalAs(UnmanagedType.Error)]
+    HRESULT CreateFindOptions([MarshalUsing(typeof(UniqueComInterfaceMarshaller<ICoreWebView2FindOptions>))] out ICoreWebView2FindOptions value);
+}
