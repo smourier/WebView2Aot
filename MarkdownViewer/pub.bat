1a6f983c05623e68f7cead25e24126595a7136a3
Simon Mourier <simon_mourier@hotmail.com>
2026-01-17 19:59:46 +0100

    wip

--- MarkdownViewer/pub.bat
+++ MarkdownViewer/pub.bat
@@ -1,9 +1,9 @@
 dotnet publish /p:Platform=x64 -p:PublishProfile=Properties\PublishProfiles\FolderProfile.x64.pubxml
-ren bin\X64\Release\net10.0\publish\win-x64\MarkdownViewer.exe bin\X64\Release\net10.0\publish\win-x64\MarkdownViewer.x64.exe
+copy bin\X64\Release\net10.0\publish\win-x64\MarkdownViewer.exe bin\X64\Release\net10.0\publish\win-x64\MarkdownViewer.x64.exe
 upx bin\X64\Release\net10.0\publish\win-x64\MarkdownViewer.x64.exe
 
 dotnet publish /p:Platform=ARM64 -p:PublishProfile=Properties\PublishProfiles\FolderProfile.ARM64.pubxml
-ren bin\ARM64\Release\net10.0\publish\win-arm64\MarkdownViewer.exe bin\ARM64\Release\net10.0\publish\win-arm64\MarkdownViewer.ARM64.exe
+copy bin\ARM64\Release\net10.0\publish\win-arm64\MarkdownViewer.exe bin\ARM64\Release\net10.0\publish\win-arm64\MarkdownViewer.ARM64.exe
 
 dotnet publish /p:Platform=x86 -p:PublishProfile=Properties\PublishProfiles\FolderProfile.x86.pubxml
-ren bin\x86\Release\net10.0\publish\win-x86\MarkdownViewer.exe bin\x86\Release\net10.0\publish\win-x86\MarkdownViewer.x86.exe
+copy bin\x86\Release\net10.0\publish\win-x86\MarkdownViewer.exe bin\x86\Release\net10.0\publish\win-x86\MarkdownViewer.x86.exe
