e782b46eadf5369ec82229264360c6d28a9db774
Simon Mourier <simon_mourier@hotmail.com>
2026-08-08 07:31:53 +0200

    Updated nuget to 1.0.4129.50

--- WebView2/Generated/WebView2/Constants.cs
+++ WebView2/Generated/WebView2/Constants.cs
@@ -3,5 +3,5 @@ namespace WebView2;
 
 public static partial class Constants
 {
-    public const string CORE_WEBVIEW_TARGET_PRODUCT_VERSION = @"150.0.4078.44";
+    public const string CORE_WEBVIEW_TARGET_PRODUCT_VERSION = @"151.0.4129.50";
 }
