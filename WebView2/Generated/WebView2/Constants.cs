166df5f309c3d1014139e5dac7a6b81de3a56113
Simon Mourier <simon_mourier@hotmail.com>
2026-08-28 13:14:51 +0200

    Updated webview2 to 1.0.4191.47

--- WebView2/Generated/WebView2/Constants.cs
+++ WebView2/Generated/WebView2/Constants.cs
@@ -3,5 +3,5 @@ namespace WebView2;
 
 public static partial class Constants
 {
-    public const string CORE_WEBVIEW_TARGET_PRODUCT_VERSION = @"151.0.4129.50";
+    public const string CORE_WEBVIEW_TARGET_PRODUCT_VERSION = @"152.0.4191.47";
 }
